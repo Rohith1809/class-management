216cc461745446e0a7130e30b32f83b6ee78c7a7
Rohith Sri Vinay Kota <121108206+Rohith1809@users.noreply.github.com>
2024-12-15 19:12:46 +0530

    Update classmanagement.sql

--- classmanagement.sql
+++ classmanagement.sql
@@ -2,10 +2,7 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 22, 2021 at 07:16 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.3.27
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
